19a3e247f2aec0d9b961e29a9061a5770e364547
Liang_Chenkang <liang_chenkang@stumail.nwu.edu.cn>
2024-01-27 20:49:35 +0800

    添加用户管理组件

--- src/views/dashboard/DashMainView.vue
+++ src/views/dashboard/DashMainView.vue
@@ -1,6 +1,15 @@
 <script setup>
 import {useMainStore} from "@/store/store.js";
 import {snackbar} from "mdui/functions/snackbar.js";
+import {ref} from 'vue';
+import UserManagement from '@/views/dashboard/components/UserManagement.vue'
+
+const currentComponent = ref(null);
+
+const showUserManagement = () => {
+  currentComponent.value = UserManagement;
+};
+
 
 </script>
 
@@ -12,49 +21,56 @@ import {snackbar} from "mdui/functions/snackbar.js";
     <!--  左边侧边栏，分为用户部分和数据部分  -->
     <mdui-card class="card-left">
       <div class="container-left">
-        <mdui-drawer id="drawer" :overlay="true" :position="'left'">
+        <mdui-drawer id="drawer" :position="'left'">
           <mdui-list>
             <!--            用户管理            -->
-            <mdui-list-item>
-              <div class="mdui-list-item-content-h" >用户管理</div>
-              <mdui-list>
-                <mdui-list-item>
-                  <div class="mdui-list-item-content">账号管理</div>
-                </mdui-list-item>
-                <mdui-list-item>
-                  <div class="mdui-list-item-content">权限变更</div>
-                </mdui-list-item>
-                <mdui-list-item>
-                  <div class="mdui-list-item-content">用户统计</div>
-                </mdui-list-item>
-              </mdui-list>
-            </mdui-list-item>
+            <div class="mdui-list-item-content-h">用户管理</div>
+            <mdui-list>
+              <mdui-list-item @click="showUserManagement">
+                <div class="mdui-list-item-content">账号管理</div>
+              </mdui-list-item>
+              <mdui-list-item>
+                <div class="mdui-list-item-content">权限变更</div>
+              </mdui-list-item>
+              <mdui-list-item>
+                <div class="mdui-list-item-content">用户统计</div>
+              </mdui-list-item>
+            </mdui-list>
+
             <!--            数据管理            -->
-            <mdui-list-item>
-              <div class="mdui-list-item-content-h">数据管理</div>
-              <mdui-list>
-                <mdui-list-item>
-                  <div class="mdui-list-item-content">实时监测</div>
-                </mdui-list-item>
-                <mdui-list-item>
-                  <div class="mdui-list-item-content">数据分析</div>
-                </mdui-list-item>
-                <mdui-list-item>
-                  <div class="mdui-list-item-content">请求管理</div>
-                </mdui-list-item>
-              </mdui-list>
-            </mdui-list-item>
+            <div class="mdui-list-item-content-h">数据管理</div>
+            <mdui-list>
+              <mdui-list-item>
+                <div class="mdui-list-item-content">实时监测</div>
+              </mdui-list-item>
+              <mdui-list-item>
+                <div class="mdui-list-item-content">数据分析</div>
+              </mdui-list-item>
+              <mdui-list-item>
+                <div class="mdui-list-item-content">请求管理</div>
+              </mdui-list-item>
+            </mdui-list>
           </mdui-list>
         </mdui-drawer>
       </div>
     </mdui-card>
-  </div>
+
+    <!-- 这个圆角我是做了一点， border-radius加了没用，我不理解 -->
+
+    <!-------------------------------------------------------------------------------------->
     <!--  右边显示数据部分  -->
-<mdui-card class="container-right">
+    <mdui-card class="card-right">
+      <div class="container-right">
+        <component :is="currentComponent"></component>
+      </div>
 
-</mdui-card>
-
+    </mdui-card>
+  </div>
 </template>
+
+
+<!--TODO 夜间模式字反色-->
+
 
 <style scoped>
 .shade {
@@ -80,9 +96,17 @@ import {snackbar} from "mdui/functions/snackbar.js";
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 80%;
+  width: 85%;
 }
 
+.container-left {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+
+}
 
 .card-left {
   position: absolute;
@@ -92,13 +116,24 @@ import {snackbar} from "mdui/functions/snackbar.js";
   z-index: 1;
 }
 
+.card-right {
+  position: absolute;
+  right: 0;
+  width: 87%;
+  height: 97%;
+  z-index: 1;
+  margin-right: 18px;
+}
+
 .mdui-list-item-content {
   font-size: 20px;
   font-weight: 500;
   display: flex;
   flex-direction: row;
   justify-content: center;
+  color: #000000;
 }
+
 .mdui-list-item-content-h {
   font-size: 15px;
   font-weight: 500;
@@ -106,5 +141,7 @@ import {snackbar} from "mdui/functions/snackbar.js";
   flex-direction: row;
   justify-content: left;
   color: #646464;
+  margin-top: 61.8%;
 }
+
 </style>
---
+++ src/views/dashboard/compopnents/UserManagement.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  // 组件的逻辑
+}
+</script>
+
+
+<template>
+  <div>
+    <!-- 用户管理组件的内容 -->
+  </div>
+</template>
+
+
+
+<style scoped>
+  /* 组件的样式 */
+</style>

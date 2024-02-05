38a3f26709e1f0707e1c55fdd0aec611b49f9211
Liang_Chenkang <liang_chenkang@stumail.nwu.edu.cn>
2024-02-06 00:45:55 +0800

    fix:账号管理组件

--- src/views/dashboard/compopnents/UserManagement.vue
+++ src/views/dashboard/compopnents/UserManagement.vue
@@ -1,48 +1,88 @@
 <!--账号管理-->
 
 <script>
-import { ref, onMounted, watch } from 'vue';
+import {ref, onMounted, watch} from 'vue';
 import axios from 'axios';
+import {useMainStore} from "@/store/store";
+import {snackbar} from "mdui";
 
 export default {
-  setup() {
-    const users = ref([]);
-    const role = ref(localStorage.getItem('role'));
-
-    onMounted(async () => {
-      if (role.value === 'admin') {
-        const token = localStorage.getItem('token');
-        const response = await axios.get('/api/get_all_users_info/', {
-          headers: {
-            'Authorization': `Bearer ${token}`
-          }
-        });
-        users.value = response.data;
-      }
-    });
-
-    watch(users, (newVal) => {
-      console.log(newVal);
-    });
-
+  data() {
     return {
-      users
-    };
+      // keyword: this.$route.params.username,
+      searchResult: null,
+      isLoading: false
+    }
+  },
+  methods: {
+    fetchData() {
+      this.isLoading = true
+      this.$axios.get('/api/get_all_users_info/', {
+        headers: {
+          'Authorization': 'Token ' + useMainStore().userInfo.token, // 请求头带上token
+          'Content-Type': 'application/json'
+        },
+        //  params: {}
+
+      }).then(
+          response => {
+            console.log(response.data);
+            if (response.data.code === 200) {
+              this.searchResult = response.data.events
+              this.isLoading = false
+            } else {
+              snackbar({
+                message: response.data.msg,
+              });
+              this.isLoading = false
+            }
+          }
+      ).catch(
+          error => {
+            console.log(error)
+            snackbar({
+              message: error,
+            });
+            this.isLoading = false
+          }
+      )
+
+    }
+
+  },
+  created() {
+    this.fetchData()
+  },
+  watch: {
+
+    $route(to, from) {
+      //this.keyword = to.params.username
+      this.fetchData()
+    }
   }
-};
+
+}
 </script>
+
+
 <template>
   <div>
     <h1>账号管理</h1>
-    <div v-for="user in users" :key="user.id">
-      <h2>{{ user.name }}</h2>
-      <p>{{ user.email }}</p>
-      <!-- 其他用户信息 -->
+    <!-- 展示用户信息 -->
+    <div v-if="searchResult">
+      <div v-for="user in searchResult">
+        <div>
+          <span>{{ user.username }}</span>
+          <span>{{ user.email }}</span>
+
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
 
 
 <style scoped>
-  /* 组件的样式 */
+/* 组件的样式 */
 </style>

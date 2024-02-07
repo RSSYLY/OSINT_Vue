09099bd85c1beec7d658a6f28b8ca72b79b5aa71
Liang_Chenkang <liang_chenkang@stumail.nwu.edu.cn>
2024-02-08 01:19:56 +0800

    fix:账号管理组件

--- src/views/dashboard/compopnents/UserManagement.vue
+++ src/views/dashboard/compopnents/UserManagement.vue
@@ -9,8 +9,8 @@ import {snackbar} from "mdui";
 export default {
   data() {
     return {
-      // keyword: this.$route.params.username,
-      searchResult: null,
+      // keyword: this.$route.params.username,  //TODO 搜索用户名
+      users: null,
       isLoading: false
     }
   },
@@ -28,7 +28,7 @@ export default {
           response => {
             console.log(response.data);
             if (response.data.code === 200) {
-              this.searchResult = response.data.events
+              this.users = response.data.events
               this.isLoading = false
             } else {
               snackbar({
@@ -68,21 +68,43 @@ export default {
 <template>
   <div>
     <h1>账号管理</h1>
+    <mdui-divider></mdui-divider>
     <!-- 展示用户信息 -->
-    <div v-if="searchResult">
-      <div v-for="user in searchResult">
-        <div>
-          <span>{{ user.username }}</span>
-          <span>{{ user.email }}</span>
-
+    <div class="list-container" v-if="users && users.length">
+      <mdui-linear-progress v-if="isLoading"></mdui-linear-progress>
+      <mdui-list v-if="!isLoading">
+        <div class="mdui-table-fluid">
+          <table class="mdui-table mdui-table-hoverable">
+            <thead>
+            <tr>
+              <th>用户名</th>
+              <th>邮箱</th>
+              <th>权限等级</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="user in users" :key="user.id">
+              <td>{{ user.username }}</td>
+              <td>{{ user.email }}</td>
+              <td>{{ user.is_superuser }}</td>
+            </tr>
+            </tbody>
+          </table>
         </div>
-      </div>
-
+      </mdui-list>
     </div>
+    <div class="error-container" v-else>
+      <mdui-icon name='error_outline'></mdui-icon>
+      <mdui-typo-display-1>没有找到相关信息</mdui-typo-display-1>
+    </div>
+
   </div>
 </template>
 
 
 <style scoped>
 /* 组件的样式 */
+.mdui-table-fluid {
+  margin: 20px;
+}
 </style>
